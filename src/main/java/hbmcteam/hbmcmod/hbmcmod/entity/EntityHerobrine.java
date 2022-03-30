9ee568c14796ae597260342fb585d1b65b66f32e
Andrew Ellen <18004410@uhi.ac.uk>
2022-03-30 19:02:12 +0100

    Changed the ai to Enderman

--- src/main/java/hbmcteam/hbmcmod/hbmcmod/entity/EntityHerobrine.java
+++ src/main/java/hbmcteam/hbmcmod/hbmcmod/entity/EntityHerobrine.java
@@ -1,39 +1,50 @@
 package hbmcteam.hbmcmod.hbmcmod.entity;
 
-import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
-public class EntityHerobrine extends EntityCow {
+public class EntityHerobrine extends EntityEnderman {
 
     public EntityHerobrine(World worldIn) {
         super(worldIn);
     }
 
     @Override
-    public EntityCow createChild(EntityAgeable ageable) {
+    public void initEntityAI() {
+        this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 100.0F));
+    }
 
-        return new EntityHerobrine(world);
+    @Override
+    public void updateAITasks() {
+
+    }
+
+    @Override
+    public boolean teleportRandomly() {
+        return false;
     }
 
     @Override
     protected SoundEvent getAmbientSound() {
 
-        return super.getAmbientSound();
+        return null;
     }
 
     @Override
     protected SoundEvent getHurtSound(DamageSource source) {
 
-        return super.getHurtSound(source);
+        return null;
     }
 
     @Override
     protected SoundEvent getDeathSound() {
 
-        return super.getDeathSound();
+        return null;
     }
 
 }
