19c836f0bc328db3084ce2211e050b5d9b44c1dd
Andrew Ellen <18004410@uhi.ac.uk>
2022-03-30 06:36:50 +0100

    Updated gradle and mcmod info

--- src/main/java/hbmcteam/hbmcmod/hbmcmod.java
+++ src/main/java/hbmcteam/hbmcmod/hbmcmod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package hbmcteam.hbmcmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = hbmcmod.MODID, name = hbmcmod.NAME, useMetadata = true, version = hbmcmod.VERSION)
+public class hbmcmod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "hbmcmod";
+    public static final String NAME = "HBMC";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
